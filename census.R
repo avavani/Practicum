2d2701a2c90637596f1de6147371327bb09d3c13
christinanmoss <cnmitchell01@gmail.com>
2025-05-04 16:53:07 -0400

    minor updates

--- census.R
+++ census.R
@@ -7,7 +7,6 @@ library(tidyr)
 options(tigris_use_cache = TRUE)
 options(tigris_class = "sf")
 
-# Set your Census API key
 census_api_key("2599edd9b7d100bcf3d6894676ccdd86672199c1", install = TRUE, overwrite = TRUE)
 
 #TIGER server down
@@ -20,7 +19,6 @@ philly_geom <- pa_geom %>%
 vars <- c(med_income = "B19013_001", med_home_value = "B25077_001")
 years <- 2015:2023
 
-# Pull ACS data for all years and bind
 acs_list <- lapply(years, function(y) {
   get_acs(
     geography = "block group",
@@ -35,12 +33,10 @@ acs_list <- lapply(years, function(y) {
 
 acs_all <- bind_rows(acs_list)
 
-# Pivot wider: one row per GEOID + year
 acs_wide <- acs_all %>%
   select(GEOID, year, variable, estimate) %>%
   pivot_wider(names_from = variable, values_from = estimate) 
 
-# Join to geometry (many-to-many: each year per GEOID)
 philly_acs <- philly_geom %>%
   left_join(acs_wide, by = "GEOID")
 
